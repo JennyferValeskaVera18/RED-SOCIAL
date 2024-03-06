98e49fc3705a22408441a6095df77a24f0607e6d
Valeska Vera <valevarillas13@gmail.com>
2024-03-06 15:40:54 -0500

    js

--- assets/js/firebase/forms.js
+++ assets/js/firebase/forms.js
@@ -7,4 +7,13 @@ document.getElementById("register-tab").addEventListener("click", function() {
     document.getElementById("login").classList.add("show", "active");
     document.getElementById("register").classList.remove("show", "active");
   });
-  
+
+  document.getElementById("logout").addEventListener("click", function() {
+    // Mostrar la pestaña de registro y la pestaña de inicio de sesión
+    document.getElementById("register").classList.add("show", "active");
+    document.getElementById("login").classList.add("show", "active");
+
+    // Asegurarse de que ambas pestañas estén visibles
+    document.getElementById("register").classList.remove("fade");
+    document.getElementById("login").classList.remove("fade");
+});
